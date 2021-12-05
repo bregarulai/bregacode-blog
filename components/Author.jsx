c552cd883668ecd4f5da771716e28f78ae77e9df
bregarulai <bregarulai@gmail.com>
2021-12-05 17:46:15 -0500

    display image in author component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Author = () => {
-  return <div>Author</div>;
+const Author = ({ author }) => {
+  return (
+    <div>
+      <img
+        src={author.photo.url}
+        alt={author.name}
+        className='align-middle rounded-full h-24 w-24'
+      />
+    </div>
+  );
 };
 
 export default Author;
