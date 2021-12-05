8a9a91e233e0a781f6815116f25dc5ead2131d60
bregarulai <bregarulai@gmail.com>
2021-12-05 12:55:10 -0500

    finished post widget component

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -22,6 +22,29 @@ const PostWidget = ({ categories, slug }) => {
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
         {slug ? 'Related Posts' : 'Recent Posts'}
       </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className='flex items-center w-full mb-4'>
+          <div className='w-16 flex-none'>
+            <img
+              src={post.featuredImage.url}
+              alt={post.title}
+              className='align-middle rounded-full h-16 w-16'
+            />
+          </div>
+          <div className='flex-grow ml-4'>
+            <p className='text-gray-500 text-xs'>
+              {moment(post.createdAt).format('MMM DD, YYYY')}
+            </p>
+            <Link
+              href={`/post/${post.slug}`}
+              key={post.title}
+              className='text-md'
+            >
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
