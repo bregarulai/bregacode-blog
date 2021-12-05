c9884f68380621b657453085203a6cfe0e58f262
bregarulai <bregarulai@gmail.com>
2021-12-05 17:59:29 -0500

    finished author component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Author = ({ author }) => {
   return (
-    <div>
-      <img
-        src={author.photo.url}
-        alt={author.name}
-        className='align-middle rounded-full h-24 w-24'
-      />
+    <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
+      <div className='absolute left-0 right-0 -top-14'>
+        <Image
+          height={100}
+          width={100}
+          unoptimized
+          src={author.photo.url}
+          alt={author.name}
+          className='align-middle rounded-full'
+        />
+      </div>
+      <h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
+      <p className='text-white text-lg'>{author.bio}</p>
     </div>
   );
 };
