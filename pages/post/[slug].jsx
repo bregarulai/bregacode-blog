37700a0af243453659b65a4cd74b572b0b45d22a
bregarulai <bregarulai@gmail.com>
2021-12-05 13:35:56 -0500

    finished structure for page details page

--- pages/post/[slug].jsx
+++ pages/post/[slug].jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
+import { getPosts, getPostsDetails } from '../../services';
+import {
+  Categories,
+  PostWidget,
+  PostDetail,
+  Author,
+  Comments,
+  CommentsForm,
+} from '../../components';
+
 const PostDetails = () => {
-  return <div>Post details page</div>;
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='col-span-1 lg:col-span-8'>
+          <PostDetail />
+          <Author />
+          <CommentsForm />
+          <Comments />
+        </div>
+        <div className='col-span-1 lg:col-span-4'>
+          <div className='relative lg:sticky top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostDetails;
