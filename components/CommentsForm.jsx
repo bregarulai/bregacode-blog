16f85b679eca3747518d0c3785b376196198e089
bregarulai <bregarulai@gmail.com>
2021-12-05 18:28:42 -0500

    added all fields and submit button to comment form component

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -8,6 +8,9 @@ const CommentsForm = ({ slug }) => {
   const nameEl = useRef();
   const emailEl = useRef();
   const storeDataEl = useRef();
+
+  const handleCommentSubmit = () => {};
+
   return (
     <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
@@ -21,7 +24,7 @@ const CommentsForm = ({ slug }) => {
           name='comment'
         />
       </div>
-      <div className='grid grid-cols-1 gap-4 mb-4'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
         <input
           type='text'
           ref={nameEl}
@@ -29,8 +32,20 @@ const CommentsForm = ({ slug }) => {
           placeholder='Name'
           name='name'
         />
+        <input
+          type='email'
+          ref={nameEl}
+          className='py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700'
+          placeholder='Email'
+          name='email'
+        />
       </div>
-      <div className='grid grid-cols-1 gap-4 mb-4'></div>
+      {error && (
+        <p className='text-xs text-red-500'>All fields are required.</p>
+      )}
+      <div className='mt-8'>
+        <button type='button' onClick={handleCommentSubmit}></button>
+      </div>
     </div>
   );
 };
