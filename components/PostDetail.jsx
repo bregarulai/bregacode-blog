feb1b47276c43b0f823b0dc2f025296e1d81869d
bregarulai <bregarulai@gmail.com>
2021-12-05 14:19:40 -0500

    added image to post detail component

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const PostDetail = () => {
-  return <div>PostDetail</div>;
+const PostDetail = ({ post }) => {
+  return (
+    <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
+      <div className='relative overflow-hidden shadow-md mb-6'>
+        <img src={post.featuredImage.url} alt={post.title} />
+      </div>
+    </div>
+  );
 };
 
 export default PostDetail;
