f4eb69b328f92374f1810b5c5f33d921e81b1273
bregarulai <bregarulai@gmail.com>
2021-12-04 19:35:34 -0500

    created getPost service

---
+++ services/index.js
@@ -0,0 +1,37 @@
+import { request, gql } from 'graphql-request';
+
+const graphqlApi = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              id
+              name
+              bio
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+  const result = await request(graphqlApi, query);
+  return result.postsConnection.edges;
+};
